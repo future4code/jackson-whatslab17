bdfe3b80dd10585a0a30c6a59b6955cb0f1c227a
RaphaelRPera <raphael.r.pera@gmail.com>
2020-08-14 20:32:30 -0300

    formulario e mensagens funcionando

--- whatslab/src/components/ComponenteTela/ComponenteTela.js
+++ whatslab/src/components/ComponenteTela/ComponenteTela.js
@@ -1,28 +1,27 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Formulario } from '../Formulario/Formulario';
-import { CompMensagem } from '../CompMensagem/CompMensagem';
 
 
 const TelaPrincipal = styled.div`
+    box-sizing: border-box;
     border: 1px solid black;
     background-color: rgb(240,240,240);
-    margin: 1vh auto auto auto;
-    width: 500px;
-    height: 98vh;
+    margin: 0 auto;
+    min-width: 300px;
+    max-width: 500px;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
 `
 
 export class ComponenteTela extends React.Component {
     render() {
         return(
-        <TelaPrincipal>
-            <CompMensagem
-                nomeUser = {'raphael'}
-                mensagem = {'Mensagem de teste'}
-            />
-            <Formulario />
-        </TelaPrincipal>
+            <TelaPrincipal>
+                <Formulario />
+            </TelaPrincipal>
         )
     }
-
 }
