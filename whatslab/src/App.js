3ed82b9667c26f8291943df925fed7ada5155d05
RaphaelRPera <raphael.r.pera@gmail.com>
2020-08-14 14:22:42 -0300

    raphael - componente tela principal criado

---
+++ whatslab/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { ComponenteTela } from './components/ComponenteTela/ComponenteTela';
+
+function App() {
+  return (
+    <div className="App">
+        <ComponenteTela>
+
+        </ComponenteTela>
+    </div>
+  )
+}
+
+export default App;
---
+++ whatslab/src/components/ComponenteTela/ComponenteTela.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const TelaPrincipal = styled.div`
+    border: 1px solid black;
+    background-color: rgb(240,240,240);
+    margin: 1vh auto auto auto;
+    width: 500px;
+    height: 98vh;
+`
+
+export class ComponenteTela extends React.Component {
+
+    render() {
+        return(
+        <TelaPrincipal>
+
+        </TelaPrincipal>
+        )
+    }
+
+}
